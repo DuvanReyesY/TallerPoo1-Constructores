189395ea1bbbb1151a80b6cefde8d50459f0ff5a
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-19 20:43:46 -0500

    friday

---
+++ Documents/NetBeansProjects/GUI1E/src/ventana/GUI1E.java
@@ -0,0 +1,14 @@
+package ventana;
+
+import javax.swing.JFrame;
+import ventana.Ventana;
+
+public class GUI1E extends JFrame{
+     
+    public static void main(String[] args) {
+       
+        Ventana form = new Ventana();
+        form.setVisible(true);
+
+    }
+}
